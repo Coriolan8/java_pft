b6586e85835a7e9b52b25c90af160f691a8b45eb
Coriolan8 <32438782+Coriolan8@users.noreply.github.com>
2018-07-16 22:38:08 +0300

    Наследование заменено на делегирование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.AplicationManager;
+
+/**
+ * Created by Yulia on 16.07.2018.
+ */
+public class TestBase {
+
+
+  protected final AplicationManager app = new AplicationManager();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    app.stop();
+  }
+
+}
