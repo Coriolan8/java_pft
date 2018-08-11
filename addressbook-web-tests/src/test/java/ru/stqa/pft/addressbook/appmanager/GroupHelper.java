d168631125b9af66cca3ca779921d0acaf580609
Coriolan8 <32438782+Coriolan8@users.noreply.github.com>
2018-08-11 21:38:04 +0300

    Преобразование кода для удаления и модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -59,8 +59,16 @@ public class GroupHelper extends HelperBase {
     fillGroupForm(group);
     submitGroupCreation();
     returnToGroupPage();
-  }
+      }
 
+
+    public void modifyGroup(int index, GroupData group) {
+    selectGroup(index);
+    initGroupModification();
+    fillGroupForm(group);
+    submitGroupModification();
+    returnToGroupPage();
+  }
   public boolean isThereAGroup() {
     return isElementPresent(By.name("selected[]"));
   }
