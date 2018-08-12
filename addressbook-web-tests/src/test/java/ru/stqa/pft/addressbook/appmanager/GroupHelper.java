41c34f366063f82e244178d9f15de46c0b72d3e1
Coriolan8 <32438782+Coriolan8@users.noreply.github.com>
2018-08-12 17:39:19 +0300

    Реализовано использование уникальных идентификаторов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Yulia on 16.07.2018.
@@ -41,8 +43,8 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value = '"+ id +"']")).click();
   }
 
   public void initGroupModification() {
@@ -62,8 +64,8 @@ public class GroupHelper extends HelperBase {
       }
 
 
-    public void modify(int index, GroupData group) {
-    selectGroup(index);
+    public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
@@ -71,8 +73,8 @@ public class GroupHelper extends HelperBase {
   }
 
 
-  public void delete(int index) {
-    selectGroup(index);
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
@@ -86,8 +88,8 @@ public class GroupHelper extends HelperBase {
 
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<>();
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
@@ -96,4 +98,6 @@ public class GroupHelper extends HelperBase {
     }
     return groups;
   }
+
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -10,6 +10,8 @@ public class GroupData {
     return id;
   }
 
+
+
   public GroupData withId(int id) {
     this.id = id;
     return this;
@@ -50,9 +52,7 @@ public class GroupData {
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
-
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -60,12 +60,14 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
-
 }
