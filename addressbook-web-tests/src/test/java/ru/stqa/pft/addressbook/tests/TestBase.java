4f1dc63457b8f841fea63e08706bfee6dbcb3c4c
Coriolan8 <32438782+Coriolan8@users.noreply.github.com>
2018-08-25 21:53:44 +0300

    Реализовано протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,16 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.AplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 /**
  * Created by Yulia on 16.07.2018.
  */
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 
   protected static final AplicationManager app
@@ -23,9 +29,20 @@ public class TestBase {
   }
 
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
+  @BeforeMethod
+  public void LogTestStart(Method m, Object[] p){
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void LogTestStop(Method m, Object[] p){
+    logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p));
+
+  }
 }
