f752bc21f078a94408a408f971c84ae08f5f1832
Coriolan8 <32438782+Coriolan8@users.noreply.github.com>
2018-09-08 17:26:27 +0300

    Pеализована интеграция тестов с баг-трекером MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -19,6 +19,7 @@ import static org.testng.Assert.assertEquals;
 public class SoapTests extends TestBase {
   @Test
   public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+    skipIfNotFixed(8);
     Set<Project> projects = app.soap().getProjects();
     System.out.println(projects.size());
     for (Project project : projects){
@@ -27,6 +28,7 @@ public class SoapTests extends TestBase {
   }
   @Test
   public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+    skipIfNotFixed(2);
     Set<Project> projects = app.soap().getProjects();
     Issue issue = new Issue().withSummary("Test issue")
             .withDescription("Test issue description")
