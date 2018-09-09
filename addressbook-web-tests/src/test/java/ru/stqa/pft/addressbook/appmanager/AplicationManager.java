f5d7b75168388a7722f0479676d51bd1e00ff285
Coriolan8 <32438782+Coriolan8@users.noreply.github.com>
2018-09-09 22:42:59 +0300

    Исправлен код

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AplicationManager.java
@@ -55,7 +55,7 @@ public class AplicationManager {
     }else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+     wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
